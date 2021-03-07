74910ef4a0b98ad86b0a1449497c745dc904829b
Fredy Yanardi <fyanardi@gmail.com>
2021-03-07 22:43:23 +0800

    A simple threadpool implementation in C++11

---
+++ debug.h
@@ -0,0 +1,46 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#if __linux
+#include <sys/syscall.h>
+#elif defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#endif
+
+/**
+ * Writes the C string pointed by format to the standard output (stdout) prefixed with other
+ * information useful in a multi-threaded environment: time and current thread id. The format
+ * accepted is the same as the format accepted by the printf function as this function also
+ * uses printf to write the final formatted output.
+ *
+ * Like printf, this is C only and does not require C++.
+ *
+ * @param format printf style C string that contains the text to be written to the stdout
+ */
+int debug(const char *format, ...) {
+    char buffer[1024];
+    char time_buf[9];
+    time_t now = time(0);
+    strftime (time_buf, 9, "%H:%M:%S", localtime (&now));
+#if __linux
+    snprintf(buffer, 1024, "[%s] [%d] %s", time_buf, syscall(__NR_gettid), format);
+#elif defined(_WIN32) || defined(_WIN64)
+    snprintf(buffer, 1024, "[%s] [%lu] %s", time_buf, GetCurrentThreadId(), format);
+#endif
+
+    char buffer1[1024];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer1, 1024, buffer, args);
+    va_end(args);
+    return printf(buffer1);
+}
+
+#endif // DEBUG_H
+
---
+++ threadpool.h
@@ -0,0 +1,180 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#if !defined(__cplusplus) || (__cplusplus < 201103L)
+#error The threadpool library requires a compiler supporting C++11.
+#endif
+
+#include <vector>
+#include <thread>
+#include <functional>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+
+#include "debug.h"
+
+/**
+ * Threadpool class implementation in C++11
+ *
+ * @author Fredy Yanardi
+ */
+class Threadpool {
+public:
+    Threadpool(int numThreads) :
+        running(true)
+    {
+        dispatcher = std::thread([this]() {
+            while (this->running) {
+                std::function<void()> f;
+                {
+                    debug("Checking job q (size: %d)\n", this->jobQ.size());
+                    std::unique_lock<std::mutex> lock1(this->mutexJobQ);
+                    while (this->jobQ.empty()) {
+                        this->cvJobQ.wait(lock1);
+                        if (!this->running) {
+                            break;
+                        }
+                    }
+
+                    if (!this->jobQ.empty()) {
+                        f = this->jobQ.front();
+                        this->jobQ.erase(this->jobQ.begin());
+                        debug("Job retrieved\n");
+                    }
+                    else {
+                        break;
+                    }
+                }
+                {
+                    debug("Checking worker idle q (size: %d)\n", this->workersQ.size());
+                    std::unique_lock<std::mutex> lock2(this->mutexIdleQ);
+                    while (this->workersQ.empty()) {
+                        this->cvIdleQ.wait(lock2); 
+                    }
+                    this->workersQ.front()->submit(f);
+                    debug("Job submitted to worker #%d\n", this->workersQ.front()->id());
+                    this->workersQ.erase(this->workersQ.begin());
+                }
+            }
+            debug("Dispatcher thread terminated\n");
+        });
+        for (int i = 0; i < numThreads; i++) {
+            int id = i;
+            Worker *worker = new Worker(i, [this, id]() {
+                debug("Worker (%d) is back to idle pool\n", id);
+                std::unique_lock<std::mutex> lock(this->mutexIdleQ);
+                this->workersQ.push_back(workers[id]);
+                this->cvIdleQ.notify_all();
+            });
+            workers.push_back(worker);
+            workersQ.push_back(worker);
+            threads.push_back(std::thread(std::ref(*worker)));
+        }
+    }
+
+    ~Threadpool()
+    {
+        for (auto worker : workers) {
+            delete worker;
+        }
+    }
+
+    void execute(const std::function<void()> &f)
+    {
+        std::unique_lock<std::mutex> lock(mutexJobQ);
+        jobQ.push_back(f);
+        cvJobQ.notify_all();
+        debug("Job submitted (job queue size: %d)\n", jobQ.size()); 
+    }
+
+    void shutdown()
+    {
+        running = false;
+        cvJobQ.notify_all();
+
+        for (auto worker : workers) {
+            worker->stop();
+        }
+
+        dispatcher.join();
+        for (auto &thread : threads) {
+            thread.join();
+        }
+
+        debug("Shutdown completed\n");
+    }
+
+private:
+    class Worker {
+    public:
+        Worker(int id, const std::function<void()> &callback) :
+            workerId(id),
+            callback(callback),
+            running(true),
+            f(NULL) {}
+
+        ~Worker() {}
+
+        void operator()()
+        {
+            while (running.load()) {
+                std::unique_lock<std::mutex> lock(mutex);
+                while (f == NULL) {
+                    cv.wait(lock);
+                    if (!running.load()) {
+                        break;
+                    }
+                }
+                if (f != NULL) {
+                    (*f)();
+                    delete f;
+                    f = NULL;
+                    callback();
+                }
+            }
+            debug("Thread (%d) terminated\n", workerId);
+        }
+
+        void submit(const std::function<void()> &f1)
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            f = new std::function<void()>(f1);
+            debug("Job submitted to thread (%d)\n", workerId);
+            cv.notify_all();
+        }
+
+        void stop()
+        {
+            running = false;
+            cv.notify_all();
+        }
+
+        int id()
+        {
+            return workerId;
+        }
+
+    private:
+        int workerId;
+        std::function<void()> callback;
+        std::atomic<bool> running;
+        std::function<void()> *f;
+        std::mutex mutex;
+        std::condition_variable cv;
+    };
+
+    std::atomic<bool> running;
+    std::thread dispatcher;
+    std::vector<std::thread> threads;
+    std::vector<Worker *> workers;
+    std::vector<Worker *> workersQ;
+    std::vector<std::function<void()>> jobQ;
+    std::mutex mutexJobQ;
+    std::mutex mutexIdleQ;
+    std::condition_variable cvJobQ;
+    std::condition_variable cvIdleQ;
+};
+
+#endif // THREADPOOL_H
+
