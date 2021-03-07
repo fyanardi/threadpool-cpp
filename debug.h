0c9f4ee60616f571cabd425f29fd505b7bd5b503
Fredy Yanardi <fyanardi@gmail.com>
2021-03-07 22:56:41 +0800

    Use milliseconds precission in debug (only in Linux for time being) and check for __unix__ instead of just __linux in debug.h

--- debug.h
+++ debug.h
@@ -7,37 +7,56 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#if __linux
+#if defined (__unix__)
+#include <math.h>
 #include <sys/syscall.h>
 #elif defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #endif
 
+#define MAX_DEBUG_BUFFER 1024
+
 /**
  * Writes the C string pointed by format to the standard output (stdout) prefixed with other
  * information useful in a multi-threaded environment: time and current thread id. The format
- * accepted is the same as the format accepted by the printf function as this function also
- * uses printf to write the final formatted output.
+ * accepted is the same as the format accepted by the printf function as this function uses
+ * vsnprintf to write the final formatted output. Thread id information is only available on
+ * either Unix or Windows environments.
  *
  * Like printf, this is C only and does not require C++.
  *
  * @param format printf style C string that contains the text to be written to the stdout
  */
 int debug(const char *format, ...) {
-    char buffer[1024];
+    char buffer[MAX_DEBUG_BUFFER];
     char time_buf[9];
-    time_t now = time(0);
-    strftime (time_buf, 9, "%H:%M:%S", localtime (&now));
-#if __linux
-    snprintf(buffer, 1024, "[%s] [%ld] %s", time_buf, syscall(__NR_gettid), format);
+
+#if defined (__unix__)
+    long ms;
+    time_t s;
+    struct timespec spec;
+
+    clock_gettime(CLOCK_REALTIME, &spec);
+
+    s = spec.tv_sec;
+    ms = spec.tv_nsec / 1.0e6;
+    strftime(time_buf, 9, "%H:%M:%S", localtime(&s));
+    snprintf(buffer, MAX_DEBUG_BUFFER, "[%s.%03ld] [%ld] %s", time_buf, ms, syscall(__NR_gettid),
+            format);
 #elif defined(_WIN32) || defined(_WIN64)
-    snprintf(buffer, 1024, "[%s] [%lu] %s", time_buf, GetCurrentThreadId(), format);
+    time_t now = time(0);
+    strftime(time_buf, 9, "%H:%M:%S", localtime(&s))
+    snprintf(buffer, MAX_DEBUG_BUFFER, "[%s] [%lu] %s", time_buf, GetCurrentThreadId(), format);
+#else
+    time_t now = time(0);
+    strftime(time_buf, 9, "%H:%M:%S", localtime(&s))
+    snprintf(buffer, MAX_DEBUG_BUFFER, "[%s] %s", time_buf, format);
 #endif
 
-    char buffer1[1024];
+    char buffer1[MAX_DEBUG_BUFFER];
     va_list args;
     va_start(args, format);
-    vsnprintf(buffer1, 1024, buffer, args);
+    vsnprintf(buffer1, MAX_DEBUG_BUFFER, buffer, args);
     va_end(args);
     return printf(buffer1);
 }
